d95d4b46ada1bca716950c84f37d982b83b619ef
shaheenshay <shaheen.shay@gmail.com>
2024-07-25 16:51:18 +0530

    registerpage submit action complete

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -8,44 +8,66 @@ import { Form, FormControl } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
-import { UserFormValidation } from "@/lib/validation";
+import { PatientFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
-import { createUser } from "@/lib/actions/patient.actions";
+import { registerPatient } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
+import {
+  Doctors,
+  GenderOptions,
+  IdentificationTypes,
+  PatientFormDefaultValues,
+} from "@/constants";
 import { Label } from "../ui/label";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
 import FileUploader from "../FileUploader";
+import { register } from "module";
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof UserFormValidation>>({
-    resolver: zodResolver(UserFormValidation),
+  const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
     defaultValues: {
+      ...PatientFormDefaultValues,
       name: "",
       email: "",
       phone: "",
     },
   });
 
-  async function onSubmit({
-    name,
-    email,
-    phone,
-  }: z.infer<typeof UserFormValidation>) {
+  async function onSubmit(values: z.infer<typeof PatientFormValidation>) {
     setIsLoading(true);
 
+    let formData;
+
+    if (
+      values.identificationDocument &&
+      values.identificationDocument.length > 0
+    ) {
+      const blobFile = new Blob([values.identificationDocument[0]], {
+        type: values.identificationDocument[0].type,
+      });
+
+      formData = new FormData();
+      formData.append("blobFile", "blobFile");
+      formData.append("fileName", values.identificationDocument[0].name);
+    }
     try {
-      const userData = { name, email, phone };
+      const patientData = {
+        ...values,
+        userId: user.$id,
+        birthDate: new Date(values.birthDate),
+        identificationDocument: formData,
+      }
+      //@ts-ignore
+      const patient = await registerPatient(patientData);
 
-      const user = await createUser(userData);
-
-      if (user) router.push(`/patients/${user.$id}/register`);
+      if(patient) router.push(`/patients/${user.$id}/new-appointment`)
     } catch (error) {
       console.log(error);
     }
@@ -299,19 +321,19 @@ const RegisterForm = ({ user }: { user: User }) => {
           control={form.control}
           name="treatmentConsent"
           label="I consent to treatment"
-        />  
+        />
         <CustomFormField
           fieldType={FormFieldType.CHECKBOX}
           control={form.control}
           name="disclosureConsent"
           label="I consent to disclosure of information"
-        />  
+        />
         <CustomFormField
           fieldType={FormFieldType.CHECKBOX}
           control={form.control}
           name="privacyConsent"
           label="I consent to privacy policy"
-        />  
+        />
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -4,7 +4,7 @@ export const {
     PROJECT_ID, API_KEY, DATABASE_ID, 
     PATIENT_COLLECTION_ID, DOCTOR_COLLECTION_ID, APPOINTMENT_COLLECTION_ID, 
     NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-    NEXT_PUBLIC_ENDPOINT: ENDPOINT
+    NEXT_PUBLIC_ENDPOINT: ENDPOINT,
 } = process.env;
 
 const client = new sdk.Client();
@@ -15,6 +15,6 @@ client
     .setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
-export const storage = new sdk.Databases(client);
+export const storage = new sdk.Storage(client);
 export const messaging = new sdk.Messaging(client);
 export const users = new sdk.Users(client);
