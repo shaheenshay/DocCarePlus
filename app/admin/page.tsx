17d9132d0d1aed3325c2d50c9de5c5423a7b8a63
shaheenshay <shaheen.shay@gmail.com>
2024-07-31 16:20:36 +0530

    admin dashboard stat card func

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,24 +1,57 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import StatCard from "@/components/StatCard";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Admin = () => {
+const Admin = async () => {
+    const appointments = await getRecentAppointmentList()
   return (
-    <div className='mx-auto flex max-w-7xl flex-col space-x-14'>
-        <header className='admin-header'>
-            <Link href="/" className='cursor-pointer'>
-                <Image 
-                    src="/assets/icons/logo-full.svg"
-                    height={32}
-                    width={132}
-                    alt="Logo"
-                    className="h-8 w-fit"                
-                />
-            </Link>
-            <p className='text-16-semibold'>Admin Dashboard</p>
-        </header>
-    </div>
-  )
-}
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={132}
+            alt="Logo"
+            className="h-8 w-fit"
+          />
+        </Link>
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
 
-export default Admin
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome 🙏</h1>
+          <p className="text-dark-700">
+            Healthcare Simplified: Smarter Scheduling, Better Care
+          </p>
+        </section>
+
+        <section className="admin-stat">
+          <StatCard 
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard 
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard 
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Cancelled appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Admin;
