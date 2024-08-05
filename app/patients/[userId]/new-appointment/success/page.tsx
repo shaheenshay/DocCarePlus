793ac493736498f07944c0a344c5d1ff0df01e1a
shaheenshay <shaheen.shay@gmail.com>
2024-08-05 12:33:51 +0530

    sms actions funtion

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -4,6 +4,9 @@ import { getAppointment } from "@/lib/actions/appointment.actions";
 import { formatDateTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import * as Sentry from '@sentry/nextjs'
+import { getUser } from "@/lib/actions/patient.actions";
+
 
 const Success = async ({
   params: { userId },
@@ -15,6 +18,11 @@ const Success = async ({
   const doctor = Doctors.find(
     (doc) => doc.name === appointment.primaryPhysician
   );
+  const user = await getUser(userId);
+
+
+  Sentry.metrics.set("user_view_appointment-success", user.name);
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,8 @@
 'use server'
 
 import { ID, Query } from "node-appwrite";
-import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, messaging, } from "../appwrite.config";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -73,7 +73,7 @@ export const getRecentAppointmentList = async () => {
     }
 }
 
-export const updateAppointment = async ({ appointmentId, userId, appointment, type}: UpdateAppointmentParams) => {
+export const updateAppointment = async ({ appointmentId, userId, appointment, type }: UpdateAppointmentParams) => {
     try {
         const updatedAppointment = await databases.updateDocument(
             DATABASE_ID!,
@@ -82,12 +82,36 @@ export const updateAppointment = async ({ appointmentId, userId, appointment, ty
             appointment
         )
 
-        if(!updatedAppointment) {
+        if (!updatedAppointment) {
             throw new Error('Appointment not found');
         }
 
+        const smsMessage = `
+          Hi, it's DocarePlus.
+          ${type === 'schedule'
+                ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!)}` : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}`
+            }   
+        `
+
+        await sendSMSNotification(userId, smsMessage);
+
         revalidatePath('/admin');
         return parseStringify(updatedAppointment)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+    try {
+        const message = await messaging.createSms(
+            ID.unique(),
+            content,
+            [],
+            [userId]
+        )
+
+        return parseStringify(message);
     } catch (error) {
         console.log(error)
     }
